9581cc3e3535c938fae844134cc30e8d2535daae
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-04-20 11:52:54 +0800

    (+) docs folder

---
+++ docs/precache-manifest.6148093b564748e63e253b10b03e1b0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ca6a41f72cb5907fefd",
+    "url": "/static/js/main.0b0e63c2.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fba0578397d7ed98a639bc1d65a75caf",
+    "url": "/index.html"
+  }
+];
