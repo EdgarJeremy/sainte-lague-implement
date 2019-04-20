d3a3ac45376f4e1615d947aaa09cb685702587aa
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-04-20 12:01:21 +0800

    (*) fix demo build

---
+++ docs/precache-manifest.98c017ad3c7b1830ef4c8bb216602fcb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1049b94a9a3f26e2c0ae",
+    "url": "/sainte-lague-implement/static/js/runtime~main.a62d8055.js"
+  },
+  {
+    "revision": "2b1e55b94ade4fc00874",
+    "url": "/sainte-lague-implement/static/js/main.6e899dcb.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/sainte-lague-implement/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "afe720883e0fd613bd1c0433ee48e5a4",
+    "url": "/sainte-lague-implement/index.html"
+  }
+];
